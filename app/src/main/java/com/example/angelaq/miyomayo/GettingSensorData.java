bc32b45f620018cdbd7eb2d41f5a9e135b7b0b2f
madelinewu <madelinewu0720@gmail.com>
2015-09-05 18:30:30 -0400

    yes

--- app/src/main/java/com/example/angelaq/miyomayo/GettingSensorData.java
+++ app/src/main/java/com/example/angelaq/miyomayo/GettingSensorData.java
@@ -18,56 +18,62 @@ import java.util.Locale;
 public class GettingSensorData extends AppCompatActivity implements SensorEventListener {
     private SensorManager sensSensorManager;
     private Sensor senAccelerometer;
+    private Sensor senGyroscope;
     private long lastUpdate = 0;
-    private float last_x,last_y,last_z;
-    private float prev_x,prev_y,prev_z;
+    private float z;
+    private float last_z;
+    private float prev_z;
     private static final int DROP_THRESHOLD = 300;
     private static final int REST_THRESHOLD = 10;
     TextToSpeech t1;
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy){}
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent){
+    public void onSensorChanged(SensorEvent sensorEvent) {
         Sensor mySensor = sensorEvent.sensor;
-        t1=new TextToSpeech(getApplicationContext(),new TextToSpeech.onInitListener() {
+        t1=new TextToSpeech(getApplicationContext(),new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                super.onInit();
+//              super.onInit();
                 if (status != TextToSpeech.ERROR) {
                     t1.setLanguage(Locale.UK);
                 }
             }
-            @Override
-            public void onInitListener() {
-                super.onInitListener();
-            }
+//            @Override
+//            public void OnInitListener() {
+//                super.OnInitListener();
+//            }
         });
         if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = sensorEvent.values[0];
-            float y = sensorEvent.values[1];
-            float z = sensorEvent.values[2];
-
+            z = sensorEvent.values[2];
             long curTime = System.currentTimeMillis();
-
             if ((curTime - lastUpdate) > 100) {
                 long diffTime = (curTime - lastUpdate);
                 lastUpdate = curTime;
-                float last_speed = Math.abs(last_x + last_y + last_z - prev_x - prev_y - prev_z) / diffTime * 10000;;
-                float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+                float last_speed = Math.abs(last_z - prev_z) / diffTime * 10000;
+                float speed = Math.abs(z - last_z) / diffTime * 10000;
                 if (last_speed > DROP_THRESHOLD && speed > REST_THRESHOLD) {
-                    Log.i("X","PHONE HAS DROPPED");
+                    Log.i("X", "PHONE HAS DROPPED");
                     CharSequence i;
                     i = "OW";
-                    t1.speak(i,TextToSpeech.QUEUE_FLUSH,null,"x");
+                    t1.speak(i, TextToSpeech.QUEUE_FLUSH, null, "x");
                 }
             }
-            prev_x = last_x;
-            prev_y = last_y;
-            prev_z = last_z;
-            last_x = x;
-            last_y = y;
-            last_z = z;
         }
+        else if (mySensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            long curr = System.currentTimeMillis();
+            if ((curr - lastUpdate) > 100) {
+                float gZ = sensorEvent.values[2];
+                if (gZ >= 4) {
+                    Log.i("Z", "PHONE HAS TURNED");
+                    CharSequence i;
+                    i = "WHEEEEEEE";
+                    t1.speak(i, TextToSpeech.QUEUE_FLUSH, null, "z");
+                }
+            }
+        }
+        prev_z = last_z;
+        last_z = z;
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +82,7 @@ public class GettingSensorData extends AppCompatActivity implements SensorEventL
         sensSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = sensSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensSensorManager.registerListener(this, senAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+        sensSensorManager.registerListener(this, senGyroscope,SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -86,13 +93,15 @@ public class GettingSensorData extends AppCompatActivity implements SensorEventL
             t1.stop();
             t1.shutdown();
         }
-        super.onPause();
+//        super.onPause();
     }
     @Override
     protected void onResume() {
         super.onResume();
-        sensSensorManager.registerListener(this,senAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+        sensSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensSensorManager.registerListener(this,senGyroscope,SensorManager.SENSOR_DELAY_NORMAL);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
