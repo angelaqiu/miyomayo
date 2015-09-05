df247865bcae456b0a76c5b735e300989ffb4645
madelinewu <madelinewu0720@gmail.com>
2015-09-05 18:29:27 -0400

    Revert "hopefully sensors work"
    This reverts commit 523b12a85785c10a95a61fbe5994a51df5c9e0d7.

--- app/src/main/java/com/example/angelaq/miyomayo/GettingSensorData.java
+++ app/src/main/java/com/example/angelaq/miyomayo/GettingSensorData.java
@@ -18,109 +18,100 @@ import java.util.Locale;
 public class GettingSensorData extends AppCompatActivity implements SensorEventListener {
     private SensorManager sensSensorManager;
     private Sensor senAccelerometer;
-    private Sensor senGyroscope;
     private long lastUpdate = 0;
-    private float z;
-    private float last_z;
-    private float prev_z;
+    private float last_x,last_y,last_z;
+    private float prev_x,prev_y,prev_z;
     private static final int DROP_THRESHOLD = 300;
     private static final int REST_THRESHOLD = 10;
     TextToSpeech t1;
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy){}
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
+    public void onSensorChanged(SensorEvent sensorEvent){
         Sensor mySensor = sensorEvent.sensor;
-        t1=new TextToSpeech(getApplicationContext(),new TextToSpeech.OnInitListener() {
+        t1=new TextToSpeech(getApplicationContext(),new TextToSpeech.onInitListener() {
             @Override
             public void onInit(int status) {
-//              super.onInit();
+                super.onInit();
                 if (status != TextToSpeech.ERROR) {
-                     t1.setLanguage(Locale.UK);
+                    t1.setLanguage(Locale.UK);
                 }
             }
-//            @Override
-//            public void OnInitListener() {
-//                super.OnInitListener();
-//            }
+            @Override
+            public void onInitListener() {
+                super.onInitListener();
+            }
         });
         if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            z = sensorEvent.values[2];
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
             long curTime = System.currentTimeMillis();
+
             if ((curTime - lastUpdate) > 100) {
                 long diffTime = (curTime - lastUpdate);
                 lastUpdate = curTime;
-                float last_speed = Math.abs(last_z - prev_z) / diffTime * 10000;
-                float speed = Math.abs(z - last_z) / diffTime * 10000;
+                float last_speed = Math.abs(last_x + last_y + last_z - prev_x - prev_y - prev_z) / diffTime * 10000;;
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
                 if (last_speed > DROP_THRESHOLD && speed > REST_THRESHOLD) {
-                    Log.i("X", "PHONE HAS DROPPED");
+                    Log.i("X","PHONE HAS DROPPED");
                     CharSequence i;
                     i = "OW";
-                    t1.speak(i, TextToSpeech.QUEUE_FLUSH, null, "x");
+                    t1.speak(i,TextToSpeech.QUEUE_FLUSH,null,"x");
                 }
             }
+            prev_x = last_x;
+            prev_y = last_y;
+            prev_z = last_z;
+            last_x = x;
+            last_y = y;
+            last_z = z;
         }
-        else if (mySensor.getType() == Sensor.TYPE_GYROSCOPE) {
-            long curr = System.currentTimeMillis();
-            if ((curr - lastUpdate) > 100) {
-                float gZ = sensorEvent.values[2];
-                if (gZ >= 4) {
-                    Log.i("Z", "PHONE HAS TURNED");
-                    CharSequence i;
-                    i = "WHEEEEEEE";
-                    t1.speak(i, TextToSpeech.QUEUE_FLUSH, null, "z");
-                }
-            }
-        }
-        prev_z = last_z;
-        last_z = z;
     }
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_getting_sensor_data);
-            sensSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-            senAccelerometer = sensSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            sensSensorManager.registerListener(this, senAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
-            sensSensorManager.registerListener(this, senGyroscope,SensorManager.SENSOR_DELAY_NORMAL);
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_getting_sensor_data);
+        sensSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = sensSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensSensorManager.registerListener(this, senAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
-        @Override
-        protected void onPause() {
-            super.onPause();
-            sensSensorManager.unregisterListener(this);
-            if (t1 != null) {
-                t1.stop();
-                t1.shutdown();
-            }
-//        super.onPause();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensSensorManager.unregisterListener(this);
+        if (t1 != null) {
+            t1.stop();
+            t1.shutdown();
         }
-        @Override
-        protected void onResume() {
-            super.onResume();
-            sensSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-            sensSensorManager.registerListener(this,senGyroscope,SensorManager.SENSOR_DELAY_NORMAL);
-        }
+        super.onPause();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensSensorManager.registerListener(this,senAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_getting_sensor_data, menu);
+        return true;
+    }
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_getting_sensor_data, menu);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
             return true;
         }
 
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
-
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                return true;
-            }
-
-            return super.onOptionsItemSelected(item);
-        }
+        return super.onOptionsItemSelected(item);
     }
+}
