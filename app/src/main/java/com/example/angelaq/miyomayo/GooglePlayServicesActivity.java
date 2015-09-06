0129032356ef9b8bac81441dd45367e9b5105206
madelinewu <madelinewu0720@gmail.com>
2015-09-05 20:14:59 -0400

    whoa does this actually build???

--- app/src/main/java/com/example/angelaq/miyomayo/GooglePlayServicesActivity.java
+++ app/src/main/java/com/example/angelaq/miyomayo/GooglePlayServicesActivity.java
@@ -15,9 +15,15 @@
  */
 package com.example.angelaq.miyomayo;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.widget.TextView;
@@ -36,6 +42,7 @@ import com.google.android.gms.fitness.data.Field;
 import com.google.android.gms.fitness.request.OnDataPointListener;
 import com.google.android.gms.fitness.result.DailyTotalResult;
 
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 
@@ -51,12 +58,71 @@ import java.util.concurrent.TimeUnit;
  * available data sources and to register/unregister listeners to those sources. It also
  * demonstrates how to authenticate a user with Google Play Services.
  */
-public class GooglePlayServicesActivity extends ActionBarActivity {
+public class GooglePlayServicesActivity extends ActionBarActivity implements SensorEventListener {
     public static final String TAG = "BasicSensorsApi";
     // [START auth_variable_references]
     private static final int REQUEST_OAUTH = 1;
 
     TextView txt;
+    private Sensor senAccelerometer;
+    private Sensor senGyroscope;
+    private long lastUpdate = 0;
+    private float z;
+    private float last_z;
+    private float prev_z;
+    private static final int DROP_THRESHOLD = 300;
+    private static final int REST_THRESHOLD = 10;
+    TextToSpeech t1;
+    private SensorManager manager;
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1) {}
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+        t1=new TextToSpeech(getApplicationContext(),new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+//              super.onInit();
+                if (status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+//            @Override
+//            public void OnInitListener() {
+//                super.OnInitListener();
+//            }
+        });
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            z = sensorEvent.values[2];
+            long curTime = System.currentTimeMillis();
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+                float last_speed = Math.abs(last_z - prev_z) / diffTime * 10000;
+                float speed = Math.abs(z - last_z) / diffTime * 10000;
+                if (last_speed > DROP_THRESHOLD && speed > REST_THRESHOLD) {
+                    Log.i("X", "PHONE HAS DROPPED");
+                    CharSequence i;
+                    i = "OW";
+                    t1.speak(i, TextToSpeech.QUEUE_FLUSH, null, "x");
+                }
+            }
+        }
+        else if (mySensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            long curr = System.currentTimeMillis();
+            if ((curr - lastUpdate) > 100) {
+                float gZ = sensorEvent.values[2];
+                if (gZ >= 4) {
+                    Log.i("Z", "PHONE HAS TURNED");
+                    CharSequence i;
+                    i = "WHEEEEEEE";
+                    t1.speak(i, TextToSpeech.QUEUE_FLUSH, null, "z");
+                }
+            }
+        }
+        prev_z = last_z;
+        last_z = z;
+    }
 
     /**
      *  Track whether an authorization activity is stacking over the current activity, i.e. when
@@ -96,8 +162,11 @@ public class GooglePlayServicesActivity extends ActionBarActivity {
         }
 
         buildFitnessClient();
-        GettingSensorData sensorData = new GettingSensorData();
-
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senGyroscope = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        manager.registerListener(this, senAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+        manager.registerListener(this, senGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
     }
     // [END auth_oncreate_setup_ending]
 
@@ -131,9 +200,9 @@ public class GooglePlayServicesActivity extends ActionBarActivity {
                                     public void onResult(DailyTotalResult dailyTotalResult) {
                                         DataSet totalSet = dailyTotalResult.getTotal();
                                         long total = totalSet.isEmpty()
-                                            ? 0
-                                            : totalSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
-                                      Log.d(TAG, "total number of steps: " + total);
+                                                ? 0
+                                                : totalSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+                                        Log.d(TAG, "total number of steps: " + total);
                                         txt.setText("# of steps:" + total);
                                     }
                                 });
@@ -202,6 +271,9 @@ public class GooglePlayServicesActivity extends ActionBarActivity {
         // Connect to the Fitness API
         Log.i(TAG, "Connecting...");
         mClient.connect();
+        manager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        manager.registerListener(this, senGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
     @Override
